8e5b9992c1b981f08bf2a1b47b3f2a2bee12d802
zplaying <profoundeye@gmail.com>
2012-08-21 12:54:24 +0800

    init
    new add

---
+++ theme/default_Blue_extend/theme.php
@@ -0,0 +1,12 @@
+<?php
+/*主题信息文件*/
+if(!defined('IN_APP')){exit('access no read');}
+$theme_info = array(
+	'skindir' => 'default_Blue_extend',
+	'name'    => '弦蓝',
+	'version' => '2.0',
+	'uri'     => 'http://qing.thinksaas.cn',
+	'author'  => 'LDuo',
+	'support' => '2.0'
+);
+
