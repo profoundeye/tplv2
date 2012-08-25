277b8733d1494bf4b24d4f59ebc9dc429753b7fe
zplaying <profoundeye@gmail.com>
2012-08-25 13:21:15 +0800

    添加新产品，template 脚本完成

---
+++ js/testData.js
@@ -0,0 +1,7 @@
+var testData = {};
+	
+// JSON string, could get this from anywhere
+testData.countryCodes = '[{"id":"AD","text":"osprey 2012年 atmos 35"},{"id":"AE","text":"burton 2011年 evo"},{"id":"AF","text":"giant 2011年 reign"}	]'
+	
+// JSON string of the above object, just as an example
+//testData.webSitesJSON = JSON.stringify(testData.webSites);
